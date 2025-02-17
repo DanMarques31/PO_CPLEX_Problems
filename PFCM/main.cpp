3bab78de7f68724fd18c9e95b9b09800f2fdb089
brunin-do-codigo <brun41.tho@gmail.com>
2025-02-17 18:36:32 -0300

    fourth commit (certo)

--- PFCM/main.cpp
+++ PFCM/main.cpp
@@ -15,28 +15,31 @@ ILOSTLBEGIN //MACRO - "using namespace" for ILOCPEX
 struct no {
     char id;
     int qntd;
+    int index;
 };
 
 struct aresta {
-    char origem, destino;
+    int origem, destino;
     int custo, fluxo_max, fluxo_min;  
 };
 
 struct grafo {
-    vector<aresta> edges;
-    vector<no> vertices;
+    int origem, destino;
+    int custo, fluxo_max;
 };
 
 int qntd_vertices, qntd_arestas;
 int qntd_oferta, qntd_demanda, qntd_transbordo;
 vector<vector<grafo>> custos;
 vector<aresta> arestas;
+vector<no> nos;
+std::map<char, int> id_to_index;
 vector<no> nos_oferta;
 vector<no> nos_demanda;
 vector<no> nos_transbordo;
 
-bool existe_no(const std::vector<no>& nos_oferta, char id_procurado) {
-    for (const no& node : nos_oferta) {
+bool existe_no(const std::vector<no>& nos, char id_procurado) {
+    for (const no& node : nos) {
         if (node.id == id_procurado) {
             return true;
         }
@@ -44,12 +47,21 @@ bool existe_no(const std::vector<no>& nos_oferta, char id_procurado) {
     return false;
 }
 
-void add_no(vector<no> &nos_oferta, char id, int qntd) {
-    no v = {id, qntd};
+int get_index(char target_id) {
+    auto it = id_to_index.find(target_id);
+    
+    if (it != id_to_index.end())
+        return it->second;
+    else
+        return -1;
+}
+
+void add_no(vector<no> &nos_oferta, char id, int qntd, int index) {
+    no v = {id, qntd, index};
     nos_oferta.push_back(v);
 }
 
-void add_aresta(vector<aresta> &arestas, char origem, char destino, int custo, int fluxo_max, int fluxo_min) {
+void add_aresta(vector<aresta> &arestas, int origem, int destino, int custo, int fluxo_max, int fluxo_min) {
     aresta e = {origem, destino, custo, fluxo_max, fluxo_min};
     arestas.push_back(e);
 }
@@ -63,54 +75,64 @@ bool read_input(const string &file_name) {
     }
     
     infile >> qntd_vertices >> qntd_arestas >> qntd_oferta >> qntd_demanda >> qntd_transbordo;
+    int index_nos = 0;
 
-    char no_origem, no_destino;
-    int custo, fluxo_max, fluxo_min;
-
-    custos.resize(256, vector<grafo>(256));
-
-    // Leitura das arestas
-    for (int i =0; i < qntd_arestas; i++) {
-        infile >> no_origem >> no_destino >> custo >> fluxo_max >> fluxo_min;
-        add_aresta(arestas, no_origem, no_destino, custo, fluxo_max, 0);
-
-        custos[no_origem][no_destino].edges.push_back({no_origem, no_destino, custo, fluxo_max, fluxo_min});
-    }
-
-    // // Matriz de custos auxiliar.
-    // for (int i = 0; i < qntd_arestas; i++) {
-    //     int origem = arestas[i].origem;
-    //     int destino = arestas[i].destino;
-    
-    //     custos[origem][destino] = arestas[i].custo;
-    //     custos[origem][destino] = arestas[i].fluxo_max;
-    // }
-
-    //Leitura dos nós de oferta
+    // Leitura dos nós de oferta.
     for (int i = 0; i < qntd_oferta; i++) {
         char id_no;
         int oferta;
         infile >> id_no >> oferta;
-
-        add_no(nos_oferta, id_no, oferta);
+        
+        add_no(nos_oferta, id_no, oferta, index_nos);
+        add_no(nos, id_no, oferta, index_nos);
+        id_to_index[id_no] = index_nos;
+        index_nos++;
     }
-
-    //Leitura dos nós de demanda
+    
+    // Leitura dos nós de demanda.
     for (int i = 0; i < qntd_demanda; i++) {
         char id_no;
         int demanda;
         infile >> id_no >> demanda;
-
-        add_no(nos_demanda, id_no, demanda);
+        
+        add_no(nos_demanda, id_no, demanda, index_nos);
+        add_no(nos, id_no, demanda, index_nos);
+        id_to_index[id_no] = index_nos;
+        index_nos++;
     }
-
+    
+    // Leitura dos nós de transbordo.
     for (int i = 0; i < qntd_transbordo; i++) {
         char id_no;
-        int demanda;
-        infile >> id_no >> demanda;
+        int transbordo;
+        infile >> id_no >> transbordo;
+        
+        add_no(nos_transbordo, id_no, transbordo, index_nos);
+        add_no(nos, id_no, transbordo, index_nos);
+        id_to_index[id_no] = index_nos;
+        index_nos++;
+    }
+    
+    char no_origem, no_destino;
+    int index_origem, index_destino;
+    int custo, fluxo_max, fluxo_min;
+    custos.resize(256, vector<grafo>(256));
 
-        add_no(nos_transbordo, id_no, demanda);
-    } 
+    // Leitura das arestas
+    for (int i = 0; i < qntd_arestas; i++) {
+        infile >> no_origem >> no_destino >> custo >> fluxo_max >> fluxo_min;
+
+        index_origem = get_index(no_origem);
+        index_destino = get_index(no_destino);
+
+        if (fluxo_max == -1)
+            add_aresta(arestas, index_origem, index_destino, custo, INT_MAX, 0);
+        else
+            add_aresta(arestas, index_origem, index_destino, custo, fluxo_max, 0);
+
+        custos[index_origem][index_destino].custo = arestas[i].custo;
+        custos[index_origem][index_destino].fluxo_max = arestas[i].fluxo_max;
+    }
 
     infile.close();
     return true;
@@ -122,10 +144,10 @@ void cplex() {
     int num_var = 0, num_res = 0;
 
     IloArray<IloNumVarArray> x(env);
-	for (int i = 0; i < qntd_arestas; i++) {
+	for (int i = 0; i < qntd_vertices; i++) {
 		x.add(IloNumVarArray(env));
 		
-        for (int j = 0; j < qntd_arestas; j++ ) {
+        for (int j = 0; j < qntd_vertices; j++ ) {
 			x[i].add(IloIntVar(env, 0, INT_MAX));
 			num_var++;
 		}
@@ -138,8 +160,9 @@ void cplex() {
     
     // FUNCAO OBJETIVO.
     obj.clear();
-    for (int i = 0; i < qntd_arestas; i++) {
-        for (int j = 0; j < qntd_arestas; j++)
+
+    for (int i = 0; i < qntd_vertices; i++) {
+        for (int j = 0; j < qntd_vertices; j++)
             obj += arestas[i].custo * x[i][j];
     }
 
@@ -150,17 +173,24 @@ void cplex() {
     // Restricao de oferta.
     for (const no& node : nos_oferta) {
         sum.clear();
-        for (const aresta& e : custos[node.id][node.id].edges) {
-            sum += x[e.origem][e.destino];
+        for (int j = 0; j < qntd_vertices; j++) {
+            if (custos[node.index][j].custo != 0)
+                sum += x[node.index][j];
         }
+        // for (const aresta& e : custos[node.id][node.id].edges) {
+        //     sum += x[e.origem][e.destino];
+        // }
         
         sum2.clear();
-        for (const aresta& e : custos[node.id][node.id].edges) {
-            sum2 += x[e.destino][e.origem];
+        for (int k = 0; k < qntd_vertices; k++) {
+            if (custos[k][node.index].custo != 0)
+                sum2 += x[k][node.index];
         }
         
-        model.add(sum - sum2 <= node.qntd);
+        model.add((sum - sum2) <= node.qntd);
         num_res++;
+
+
         // for (int i = 0; i < qntd_vertices; i++) {
         //    sum.clear();
     
@@ -186,38 +216,46 @@ void cplex() {
     // Restricao de demanda.
     for (const no& node : nos_demanda) {
         sum.clear();
-        for (const aresta& e : custos[node.id][node.id].edges) {
-            sum += x[e.origem][e.destino];
-        }
-        
+        for (int j = 0; j < qntd_vertices; j++) {
+            if (custos[node.index][j].custo != 0)
+                sum += x[node.index][j];
+        } 
+
         sum2.clear();
-        for (const aresta& e : custos[node.id][node.id].edges) {
-            sum2 += x[e.destino][e.origem];
+        for (int k = 0; k < qntd_vertices; k++) {
+            if (custos[k][node.index].custo != 0)
+                sum2 += x[k][node.index];
         }
-        
-        model.add(sum - sum2 <= node.qntd);
+
+        model.add((sum - sum2) <= -node.qntd);
         num_res++;
     }
 
-    // Restricao de conservação de fluxo.
+    // // Restricao de conservação de fluxo.
     for (const no& node : nos_transbordo) {
         sum.clear();
-        for (const aresta& e : custos[node.id][node.id].edges) {
-            sum += x[e.origem][e.destino];
-        }
-        
+        for (int j = 0; j < qntd_vertices; j++) {
+            if (custos[node.index][j].custo != 0)
+                sum += x[node.index][j];
+        }   
+
         sum2.clear();
-        for (const aresta& e : custos[node.id][node.id].edges) {
-            sum2 += x[e.destino][e.origem];
-        }
+        for (int k = 0; k < qntd_vertices; k++) {
+            if (custos[k][node.index].custo != 0)
+                sum2 += x[k][node.index];
+        } 
         
-        model.add(sum - sum2 <= node.qntd);
+        model.add((sum - sum2) == 0);
         num_res++;
     }
+
+    // // Restricao de capacidades
     for (int i = 0; i < qntd_arestas; i++) {
         for (int j = 0; j < qntd_arestas; j++) {
-            model.add(x[i][j] <= arestas[i].fluxo_max);
-            model.add(x[i][j] >= arestas[i].fluxo_min);
+            if (custos[i][j].fluxo_max != 0) {
+                model.add(x[i][j] <= custos[i][j].fluxo_max);
+                num_res++;
+            }
         }
     }
 
@@ -266,7 +304,10 @@ void cplex() {
         cout << "Variaveis de decisao: " << endl;
 		for(int i = 0; i < qntd_vertices; i++) {
 		    for(int j = 0; j < qntd_vertices; j++) {
-			    value = IloRound(cplex.getValue(x[i][j]));
+                // printf("\n\nCUSTO: %d\n\n", custos[i][j].edges[i].custo);
+			    if (custos[i][j].custo == 0)
+                    continue;
+                value = IloRound(cplex.getValue(x[i][j]));
 			    printf("x[%d][%d]: %.0lf\n", i, j, value);
             }
 		}
@@ -286,19 +327,19 @@ void cplex() {
 	env.end();
 }
 
-
 int main() {    
     if (!read_input("in.txt"))
         cout << "Erro.";
     
     printf("Verificacao da leitura dos dados:\n");
+    printf("Quantidade de vertices: %d\n", qntd_vertices);
 	printf("Quantidade de arestas: %d\n", qntd_arestas);
 	printf("Quantidade de nós de oferta: %d\n", qntd_oferta);
 	printf("Quantidade de nós de demanda: %d\n", qntd_demanda);
 	printf("Quantidade de nós de transbordo: %d\n", qntd_transbordo);
 	printf("Origem - Destino - Custo - Fluxo máximo - Fluxo mínimo\n");
     for(int i = 0; i < qntd_arestas; i++)
-        printf("%c %c %d %d %d\n", arestas[i].origem, arestas[i].destino, arestas[i].custo, arestas[i].fluxo_max, arestas[i].fluxo_min);
+        printf("%d %d %d %d %d\n", arestas[i].origem, arestas[i].destino, arestas[i].custo, arestas[i].fluxo_max, arestas[i].fluxo_min);
     printf("\n");
     for(int i = 0; i < qntd_oferta; i++) {
         printf("%c %d\n", nos_oferta[i].id, nos_oferta[i].qntd);
